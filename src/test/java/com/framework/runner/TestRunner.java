767ac92f99096d1bf0ab6f860817e881ca31aa87
ravivce321-dev <ravivce321@gmail.com>
2026-02-01 13:23:07 +0530

    removed unused import

--- src/test/java/com/framework/runner/TestRunner.java
+++ src/test/java/com/framework/runner/TestRunner.java
@@ -2,7 +2,6 @@ package com.framework.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.TestNGCucumberRunner;
 import org.testng.annotations.DataProvider;
 
 // Cucumber Runner Configuration
